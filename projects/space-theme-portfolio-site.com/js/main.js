d5a4cb8a7f3ab6c8f76e14770d324902c5ebd724
Ryan <39573308+RyanThen@users.noreply.github.com>
2024-03-18 17:28:25 -0500

    add old space theme portfolio site

---
+++ projects/space-theme-portfolio-site.com/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    
+
+// Smooth Scroll To Anchor Tags
+    $('a[href^="#"]').on('click', function(e){
+        e.preventDefault();
+        
+        var target = this.hash;
+        var $target = $(target);
+        
+        $('html, body').animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing');
+    }); 
+    
+    
+    
+});  //End Document Ready
